147d730c64070f68fbe08123e5000282d054dc9f
QuangVann <115283300+QuangVann@users.noreply.github.com>
2023-05-27 14:22:18 +0700

    FE: add_product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { AdminPagesComponent } from './admin/admin-pages/admin-pages.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
+import { ProductEditComponent } from './components/product-edit/product-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
+  { path: 'admin', component: AdminPagesComponent },
+  { path: 'admin/product', component: AdminPagesComponent },
+  { path: 'admin/add', component: ProductAddComponent },
+  { path: 'admin/product/edit/:id', component: ProductEditComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
